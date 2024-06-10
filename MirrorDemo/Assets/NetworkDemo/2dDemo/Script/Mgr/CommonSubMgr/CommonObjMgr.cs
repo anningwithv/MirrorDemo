bad8d499762a7ac350cc61957884f863d4681284
v <378294113@qq.com>
2024-06-10 19:00:41 +0800

    update

--- MirrorDemo/Assets/NetworkDemo/2dDemo/Script/Mgr/CommonSubMgr/CommonObjMgr.cs
+++ MirrorDemo/Assets/NetworkDemo/2dDemo/Script/Mgr/CommonSubMgr/CommonObjMgr.cs
@@ -35,22 +35,22 @@ public class CommonObjMgr : CommonSubMgr
         string path1 = $"{m_ObjPath}Bullet";
         GameObject bullet = Resources.Load(path1) as GameObject;
         //NetworkClient.RegisterPrefab(bullet, SpawnHandler, UnspawnHandler);
-        NetworkClient.RegisterPrefab(bullet);
+        //NetworkClient.RegisterPrefab(bullet);
         GameObjectPoolMgr.S.AddPool(bullet.name, bullet, 100, 10);
 
         string path2 = $"{m_ObjPath}BulletTower";
         GameObject bulletTower = Resources.Load(path2) as GameObject;
-        NetworkClient.RegisterPrefab(bulletTower);
+        //NetworkClient.RegisterPrefab(bulletTower);
         GameObjectPoolMgr.S.AddPool(bulletTower.name, bulletTower, 100, 10);
 
         string path3 = $"{m_ObjPath}Enemy";
         GameObject enemy = Resources.Load(path3) as GameObject;
-        NetworkClient.RegisterPrefab(enemy);
+        //NetworkClient.RegisterPrefab(enemy);
         GameObjectPoolMgr.S.AddPool(enemy.name, enemy, 100, 10);
 
         string path4 = $"{m_ObjPath}Bullet2";
         GameObject bullet2 = Resources.Load(path4) as GameObject;
-        NetworkClient.RegisterPrefab(bullet2);
+        //NetworkClient.RegisterPrefab(bullet2);
         GameObjectPoolMgr.S.AddPool(bullet2.name, bullet2, 100, 10);
     }
     //private GameObject SpawnHandler(SpawnMessage msg)
